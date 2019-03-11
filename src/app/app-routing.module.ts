1a858a6f6f8e7edf4844f6ca72b9f58d605873ed
ishman <ishman99@gmail.com>
2019-03-11 11:17:02 +0100

    Cambio de la pantalla principal de la app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,10 @@
+import { RouterModule, Routes } from '@angular/router';
+
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: './home/home.module#HomePageModule' },
+  { path: '', redirectTo: 'inicio', pathMatch: 'full' },
+  { path: 'inicio', loadChildren: './pages/inicio/inicio.module#InicioPageModule' }
 ];
 
 @NgModule({
