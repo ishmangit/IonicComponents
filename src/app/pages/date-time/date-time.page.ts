82d528c67473ea0afd6c36e83fcf88032efaeb69
ishman <ishman99@gmail.com>
2019-03-12 11:06:13 +0100

    Customización de datetime

--- src/app/pages/date-time/date-time.page.ts
+++ src/app/pages/date-time/date-time.page.ts
@@ -8,10 +8,26 @@ import { Component, OnInit } from '@angular/core';
 export class DateTimePage implements OnInit {
 
   fechaNac: Date = new Date();
+  customPickerOptions;
+  customDate;
 
   constructor() { }
 
   ngOnInit() {
+    this.customPickerOptions = {
+      buttons: [{
+        text: 'Save',
+        handler: (evento) => {
+          console.log(evento);
+        }
+      }, {
+        text: 'Log',
+        handler: () => {
+          console.log('Clicked Log. Do not Dismiss.');
+          return false;
+        }
+      }]
+    };
   }
 
   cambioFecha(event) {
