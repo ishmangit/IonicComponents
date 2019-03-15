da7df232b6e5293d7db8d9c11a50d2c7075eff67
ishman <ishman99@gmail.com>
2019-03-15 11:51:38 +0100

    Añadido sliding a la lista

--- src/app/pages/list/list.page.ts
+++ src/app/pages/list/list.page.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 import { DataService } from 'src/app/services/data.service';
+import { IonList } from '@ionic/angular';
 import { Observable } from 'rxjs';
 
 @Component({
@@ -10,12 +11,27 @@ import { Observable } from 'rxjs';
 })
 export class ListPage implements OnInit {
 
+  @ViewChild('lista') lista: IonList;
+
   usuarios: Observable<any>;
 
   constructor(private dataService: DataService) { }
 
   ngOnInit() {
     this.usuarios = this.dataService.getUsers();
+  }
+
+  favorite(usuario: any) {
+    console.log('favorite: ', usuario);
+    this.lista.closeSlidingItems();
+  }
+  share(usuario: any) {
+    console.log('share: ', usuario);
+    this.lista.closeSlidingItems();
+  }
+  borrar(usuario: any) {
+    console.log('borrar: ', usuario);
+    this.lista.closeSlidingItems();
   }
 
 }
