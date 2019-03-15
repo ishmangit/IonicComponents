30602c955bc55ae2e8ef942b07928ab6537b97e0
ishman <ishman99@gmail.com>
2019-03-15 10:54:48 +0100

    Implementado Input y Form sencillo

---
+++ src/app/pages/input/input.module.ts
@@ -0,0 +1,27 @@
+import { RouterModule, Routes } from '@angular/router';
+
+import { CommonModule } from '@angular/common';
+import { ComponentsModule } from 'src/app/components/components.module';
+import { FormsModule } from '@angular/forms';
+import { InputPage } from './input.page';
+import { IonicModule } from '@ionic/angular';
+import { NgModule } from '@angular/core';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: InputPage
+  }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    RouterModule.forChild(routes),
+    ComponentsModule
+  ],
+  declarations: [InputPage]
+})
+export class InputPageModule {}
