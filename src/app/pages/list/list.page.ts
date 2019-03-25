aca4f4cfbd716913a804549960de3bf3c836422f
ishman <ishman99@gmail.com>
2019-03-25 11:46:05 +0100

    Implementados ejemplos de toast

--- src/app/pages/list/list.page.ts
+++ src/app/pages/list/list.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
+import { IonList, ToastController } from '@ionic/angular';
 
 import { DataService } from 'src/app/services/data.service';
-import { IonList } from '@ionic/angular';
 import { Observable } from 'rxjs';
 
 @Component({
@@ -15,22 +15,34 @@ export class ListPage implements OnInit {
 
   usuarios: Observable<any>;
 
-  constructor(private dataService: DataService) { }
+  constructor(private dataService: DataService,
+              private toastCtrl: ToastController) { }
 
   ngOnInit() {
     this.usuarios = this.dataService.getUsers();
   }
 
+  async presentToast(message: string) {
+    const toast = await this.toastCtrl.create({
+      message,
+      duration: 2000
+    });
+    toast.present();
+  }
+
   favorite(usuario: any) {
-    console.log('favorite: ', usuario);
+    // console.log('favorite: ', usuario);
+    this.presentToast('Se ha guardado en favoritos');
     this.lista.closeSlidingItems();
   }
   share(usuario: any) {
-    console.log('share: ', usuario);
+    // console.log('share: ', usuario);
+    this.presentToast('Elemento compartido');
     this.lista.closeSlidingItems();
   }
   borrar(usuario: any) {
-    console.log('borrar: ', usuario);
+    // console.log('borrar: ', usuario);
+    this.presentToast('Elemento borrado');
     this.lista.closeSlidingItems();
   }
 
